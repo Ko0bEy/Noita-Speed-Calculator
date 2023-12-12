b321bf2080b8a2c196b672cad85644da794e4826
nico_ <nico_steinberg@hotmail.de>
2023-12-12 14:40:10 +0100

    initial commit

---
+++ speed_calc.py
@@ -0,0 +1,228 @@
+import numpy as np
+from argparse import ArgumentParser
+from collections import defaultdict
+from tqdm import tqdm
+import multiprocessing
+
+from utils import (
+    get_starting_nodes,
+    calculate_fly_up_multipliers,
+    check_solution,
+    print_results,
+    extend_coefs,
+    choose_increase_decrease,
+    choose_wisely,
+    set_globals,
+    get_multiplier
+)
+
+
+def initialize(args):
+#    n_processes = args.p
+    strategy = args.s
+    coefs = tuple(float(coef) for coef in args.coefs)
+    specials_idx, extended_coefs = extend_coefs(coefs)
+    set_globals(coefs)
+    # Search parameters
+    max_iter, budget_weight = 0, 0
+    mode = args.m
+    if mode == "shallow":
+        max_iter = 3
+    elif mode == "normal":
+        max_iter = 6
+    elif mode == "deep":
+        max_iter = 9
+    elif mode == "verydeep":
+        max_iter = 18
+
+    if args.iter is not None:
+        max_iter = args.iter
+
+    budget = args.b
+    max_rel_err = np.inf
+
+    if budget == "budget":
+        budget_weight = 2.0
+        max_rel_err = 0.035
+    elif budget == "normal":
+        budget_weight = 1.0
+        max_rel_err = 0.01
+    elif budget == "accurate":
+        budget_weight = 0.5
+        max_rel_err = 0.001
+    elif budget == "ignorebudget":
+        budget_weight = 0.0
+        max_rel_err = 0.001
+
+    if args.w is not None:
+        budget_weight = args.w
+
+    if args.e is not None:
+        max_rel_err = args.e
+
+    distance = args.distance
+    if args.min is None:
+        dmin = (1 - max_rel_err) * distance
+    else:
+        dmin = args.min
+    if args.max is None:
+        dmax = (1 + max_rel_err) * distance
+    else:
+        dmax = args.max
+    v0 = args.v0
+    target_multiplier = distance / v0
+    min_multiplier = dmin / v0
+    max_multiplier = dmax / v0
+
+    return (
+        distance,
+        coefs,
+        dmin,
+        dmax,
+        v0,
+        max_rel_err,
+        max_iter,
+        target_multiplier,
+        min_multiplier,
+        max_multiplier,
+        budget,
+        budget_weight,
+        strategy,
+        specials_idx,
+        extended_coefs,
+    )
+
+#@profile
+def _search(
+    target,
+    target_min,
+    target_max,
+    coefs=(0.3, 0.32, 0.33, 0.75, 1.68, 2, 2.5, 7.5),
+    max_iter=10,
+    strategy="complete",
+    n_processes=1,
+):
+    #@profile
+    #@functools.cache
+    def _step(node, path):
+        candidates = candidate_chooser(node, target)
+        new_solutions = set()
+        for a, c in candidates:
+            new_path = list(path)
+            new_path[coefs.index(c)] += a
+            #new_solution = (node * c**a, tuple(new_path))
+            new_solution = (get_multiplier(new_path), tuple(new_path))
+            new_solutions.add(
+                (new_solution, check_solution(new_solution[0], target_min, target_max))
+            )
+        return new_solutions
+
+    # extension_start_idx, extended_coefs = extend_coefs(coefs)
+    min_flyups, fly_up_multipliers = calculate_fly_up_multipliers(target)
+    pool = multiprocessing.Pool(processes=n_processes)
+    candidate_chooser = None
+    if strategy == "complete":
+        candidate_chooser = choose_increase_decrease
+    else:
+        assert strategy == "wise"
+        candidate_chooser = choose_wisely
+
+    last_nodes = get_starting_nodes(min_flyups, fly_up_multipliers)
+    current_nodes = set()
+    #     # debu_path = np.asarray([0, 0, 17, 1, 0, 1, 0, 1, 189])
+    for iter in tqdm(range(max_iter), total=max_iter):
+        solutions = defaultdict(set)
+        for node, path in last_nodes:
+            for (new_node, new_path), check in _step(node, path):
+                current_nodes.add((new_node, new_path))
+                if check:
+                    solutions[new_node].add(new_path)
+        last_nodes = current_nodes
+        current_nodes = set()
+        yield solutions
+    # return solutions
+
+#@profile
+def find_solutions(*args):
+    solutions = defaultdict(set)
+    for solution_batch in _search(*args):
+        for multiplier, paths in solution_batch.items():
+            node = multiplier
+            solutions[node] = solutions[node].union(paths)
+    return solutions
+
+
+def main():
+    global coefs
+    parser = ArgumentParser()
+    parser.add_argument("distance", default=35840, type=int)
+    parser.add_argument(
+        "-mode",
+        "--m",
+        default="shallow",
+        required=False,
+        type=str,
+        choices=["normal", "shallow", "deep", "verydeep"],
+    )
+    parser.add_argument(
+        "-budget",
+        "--b",
+        default="normal",
+        required=False,
+        type=str,
+        choices=["accurate", "normal", "budget", "ignorebudget"],
+    )
+    parser.add_argument("-distance_min", "--min", type=int)
+    parser.add_argument("-distance_max", "--max", type=int)
+    parser.add_argument("-v0", "--v0", default=7.92, type=float)
+    parser.add_argument("-err", "--e", type=float, default=0.035)
+    parser.add_argument("-weight", "--w", type=float)
+    parser.add_argument(
+        "-strategy", "--s", type=str, choices=["complete", "wise"], default="wise"
+    )
+    parser.add_argument(
+        "-coefs", nargs="+", default=(0.3, 0.32, 0.33, 0.75, 1.68, 2, 2.5, 7.5)
+    )
+    parser.add_argument("-iter_max", "--iter", type=int)
+
+    args = parser.parse_args()
+
+    (
+        distance,
+        coefs,
+        dmin,
+        dmax,
+        v0,
+        max_rel_err,
+        max_iter,
+        target_multiplier,
+        min_multiplier,
+        max_multiplier,
+        budget,
+        budget_weight,
+        strategy,
+        specials_idx,
+        extended_coefs,
+    ) = initialize(args)
+
+    solutions = find_solutions(
+        target_multiplier,
+        min_multiplier,
+        max_multiplier,
+        coefs,
+        max_iter,
+        strategy,
+    )
+
+    if solutions:
+        print_results(
+            solutions,
+            50,
+            distance,
+            target_multiplier,
+            budget_weight,
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,310 @@
+import numpy as np
+import pickle
+import functools
+
+# floating point precisiion (i think)
+epsilon = 1.175494351e-38
+
+# somewhat arbitrary measure of how hard numbers are to divide by
+divide_chain_difficulty = pickle.load(open("divide_chain_difficulty.pkl", "rb"))
+
+# initialize some globals to be set by set_globals. Makes some things easier and/or faster.
+coefs = []
+log_coefs = []
+increasers = []
+decreasers = []
+increaser_candidates = set()
+decreaser_candidates = set()
+extended_coefs = []
+extension_idx = 0
+
+# call this once to set up the right values for the utility functions
+def set_globals(_coefs: tuple[float], special_coefs: tuple[float] = (1.2,)) -> None:
+    """
+    call this once to set up the right values for the utility functions.
+    :param _coefs: modifiers to use
+    :param special_coefs: uncapped modifiers to use (Fly Upwards/Downwards, Faster Projectiles)
+    :return: None
+    """
+    global log_coefs, increasers, decreasers, coefs, extended_coefs, extension_idx, increaser_candidates, decreaser_candidates
+    coefs = _coefs
+    extension_idx, extended_coefs = extend_coefs(coefs, special_coefs)
+    log_coefs = np.log(_coefs).tolist()
+    increasers = tuple(x for x in _coefs if x > 1)
+    decreasers = tuple(x for x in _coefs if x < 1)
+    increaser_candidates = set((1,c) for c in increasers)
+    decreaser_candidates = set((1,c) for c in decreasers)
+
+
+
+def rel_error(val: float, tar: float) -> float:
+    """
+    relative error between val and tar
+    :param val: value
+    :param tar: target
+    :return: relative error
+    """
+    return np.abs(val / tar - 1)
+
+
+@functools.cache
+def difficulty(path, budget_weight, specials_idx):
+    """
+    Somewhat arbitrary measure of how hard a solution is to build. Used to sort solutions.
+    :param path: list of modifier amounts
+    :param budget_weight: how much weight should be given to the difficulty for the overall score
+    :param specials_idx: where do capped modifiers stop, and uncapped modifiers start
+    :return:
+    """
+    return np.round(
+        budget_weight
+        * (
+            +sum(
+                divide_chain_difficulty[p] ** 1.5
+                if i < specials_idx
+                else divide_chain_difficulty[p]
+                for i, p in enumerate(path)
+            )
+            + (np.count_nonzero(path) - 1) * np.count_nonzero(path) / 2
+        ),
+        2,
+    )
+
+@functools.cache
+def error_bonus(value, target, cap=np.inf):
+    """
+    Rewards solutions for accuracy
+    :param value: speed multiplier of the solution
+    :param target: target speed multiplier
+    :param cap: max bonus awarded for accuracy
+    :return:
+    """
+    score = np.round(np.log(1 / rel_error(value, target) ** 1.8), 2)
+    return min(score, cap)
+
+
+@functools.cache
+#@profile
+def choose_wisely(node, target) -> set[tuple[int]]:
+    """
+    from a given path and speed multiplier, choose modifiers and corresponding amounts to add, and check in the next iteration
+    :param node: speed multiplier
+    :param target: target speed multiplier
+    :return: a set of tuples containing the modifier and amount to add
+    """
+    relation = target / node
+    amounts_candidates = set()
+    if coefs[0] < relation < coefs[-1]:
+        return choose_increase_decrease(node, target)
+    else:
+        for amount, candidate in choose_increase_decrease(node, target):
+            amount = int(np.log(relation) / log_coefs[coefs.index(candidate)])
+            amounts_candidates = amounts_candidates.union({(amount, candidate), (amount + 1, candidate)})
+    return amounts_candidates
+
+
+@functools.cache
+#@profile
+def choose_increase_decrease(node, target) -> set[tuple[int]]:
+    """
+    chooses modifiers via a simple heuristic, and adds 1 of each for the next iteration.
+    :param node: speed multiplier
+    :param target: target speed multiplier
+    :return: a set of tuples containing the modifier and amount to add
+    """
+    if node < target:
+        return increaser_candidates
+    elif node > target:
+        return decreaser_candidates
+
+@functools.cache
+def path_cost(value: float, path: tuple[int], target: float, budget_weight:float, specials_idx:int, acc_cap:float=np.inf) ->  float:
+    """
+    How good is the solution, regarding difficulty to build, and accuracy
+    :param value: speed multiplier
+    :param path: modifier amounts
+    :param target: target speed multiplier
+    :param budget_weight: weight of the difficulty score
+    :param specials_idx: where do uncapped modifiers start
+    :param acc_cap: max points awarded for accuracy
+    :return:
+    """
+    diff = difficulty(path, budget_weight, specials_idx)
+    err = error_bonus(
+        value, target, cap=acc_cap
+    )
+    return diff - err
+
+
+def with_sign(s: float) -> str:
+    """
+    adds a plus sign to positive values
+    :param s: value
+    :return: string, for example +0.43
+    """
+    s = float(s)
+    if s >= 0:
+        return "+" + str(s)
+    else:
+        return str(s)
+
+
+def to_equation(sol: tuple[int,tuple[int]], distance:int, target:float, budget_weight:float, specials_idx:int) -> str:
+    """
+    Returns a solution in a human-readable form
+    :param sol: solution (speed_multiplier, path)
+    :param distance: desired distance to travel
+    :param target: target speed multiplier
+    :param budget_weight: weight of difficulty score
+    :param specials_idx: where do uncapped modifiers start
+    :return:
+    """
+    message = ""
+    for i, c in enumerate(extended_coefs):
+        if sol[1][i] != 0:
+            term = f"{c:.2f}^{sol[1][i]}"
+            term = term + " " * (8 - len(term)) + " * "
+            message += term
+        else:
+            message += " " * 9 + "* "
+    message = (
+        message[:-2]
+        + f"=\t{np.round(sol[0], 5)}\t({with_sign(int(get_distance(sol[0]) - distance))}px) \t({with_sign(100 * rel_error(sol[0], target))}%)\t({difficulty(sol[1], budget_weight, specials_idx)})\t\t({error_bonus(sol[0],target)})\t\t({path_cost(*sol, target, budget_weight, specials_idx):.2f})"
+    )
+    return message
+
+
+def print_headline():
+    print(
+        "HeavyS     Accel      Phasing    ExploP     Decel      Chaotic    Speed+     LightS     FlyUp\t\tSpeedMul\t\tError\t\t\t%-Error\t\t\tDifficulty\tAccuracy\tQuality (lower=better)"
+    )
+
+
+def print_results(
+    solutions: dict, n: int, distance: int, target: float, budget_weight: float
+) -> None:
+    """
+    sorts the results, and prints the output
+    :param solutions: solutions
+    :param n: number of solutions to print
+    :param distance: desired distance
+    :param target: target speed mutiplier
+    :param budget_weight: weight of the difficulty
+    :return: None
+    """
+    solutions = sort_solutions(
+        solutions,
+        lambda val, path: path_cost(val, path, target, budget_weight, extension_idx),
+    )
+    print()
+    print_headline()
+    for sol in solutions[:n]:
+        print(
+            to_equation(
+                sol, distance, target, budget_weight, extension_idx
+            )
+        )
+
+
+
+def extend_coefs(_coefs, special_coefs=(1.2,))-> tuple[int, tuple]:
+    """
+    attaches uncapped modifiers to the end of the list
+    :param _coefs: capped modifiers
+    :param special_coefs: uncapped modifiers
+    :return:
+    """
+    extended_coefs = tuple(list(_coefs) + list(special_coefs))
+    return len(_coefs), extended_coefs
+
+
+def sort_solutions(tree: dict[float:set[tuple[int]]], scorer: callable, reverse: bool=False) -> list[tuple]:
+    """
+    sorts the solutions based on the provided scorer
+    :param tree: solutions
+    :param scorer: function to use for scoring
+    :param reverse: sort in reverse?
+    :return: sorted list of sollutions
+    """
+    return sorted(
+        (
+            (multiplier, path)
+            for multiplier in tree
+            for path in tree[multiplier]
+            # if sum(1 for s in sol if s != 0) <= 3
+        ),
+        key=lambda x: scorer(*x),
+        reverse=reverse,
+    )
+
+
+@functools.cache
+def check_solution(multiplier: float, target_min:float, target_max:float) -> bool:
+    """
+    check whether a given multiplier falls in the desired range
+    :param multiplier: speed multiplier
+    :param target_min: minimum speed multiplier
+    :param target_max: maximum speed multiplier
+    :return: True/False
+    """
+    return target_min < multiplier < target_max
+
+
+def get_starting_nodes(min_flyups: int, fly_up_multipliers: list[float])-> set[tuple[float, tuple]]:
+    """
+    return the set of potential solutions to start the search with.
+    :param min_flyups: how many of the uncapped modifier do we have to use at least?
+    :param fly_up_multipliers: pre-calculated list of possible multipliers using the uncapped modifier
+    :return:
+    """
+    nodes = set()
+    for i, m in enumerate(fly_up_multipliers):
+        flyup_count = min_flyups + i
+        path = tuple([0 for _ in extended_coefs[:-1]] + [flyup_count])
+        nodes.add((get_multiplier(path), path))
+    return nodes
+
+
+def get_multiplier(path: tuple[int]) -> float:
+    """
+    calculates speed multiplier from a given list of modifier amounts
+    :param path: modifier amounts
+    :return: speed multiplier
+    """
+    return float(np.prod([c ** path[i] for i, c in enumerate(extended_coefs)], dtype=np.float32))
+
+
+def get_distance(multiplier: float, v0: float=7.92) -> float:
+    """
+    calculates the distance traveled, using a give speed multiplier
+    :param multiplier: speed multiplier
+    :param v0: effective base speed (tentacle: 7.92)
+    :return: distance traveled
+    """
+    return multiplier * v0
+
+
+def calculate_fly_up_multipliers(target_multiplier: float, max_len: int=200) -> tuple[int, np.array]:
+    """
+    Pre-calculates all possible multipliers reached by the uncapped modfier
+    :param target_multiplier: target multiplier
+    :param max_len: maximum amount of entries to be generated
+    :return: minimum amount of uncapped modifiers needed, array of multipliers
+    """
+    fly_up_multipliers = []
+    cnt = 0
+    res = 1
+    min_count = None
+    while True:
+        cnt += 1
+        res = res * 1.2
+        if 20 >= target_multiplier / res >= epsilon:
+            if min_count is None:
+                min_count = cnt
+            fly_up_multipliers.append(res)
+        elif target_multiplier / res < epsilon:
+            break
+        if len(fly_up_multipliers) >= max_len:
+            break
+    fly_up_multipliers = np.flip(np.asarray(fly_up_multipliers))
+    return min_count, fly_up_multipliers
